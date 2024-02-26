0a54c32be759ba28fd649f22b292f64e14d8942a
Fabio Neves <fabinhoneves09@gmail.com>
2024-02-26 08:20:12 -0300

    Done

--- App.tsx
+++ App.tsx
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [image, setImage] = useState(
+    "https://reactnative.dev/docs/assets/p_cat2.png",
+  );
+  const [bgColor, setBgColor] = useState("white");
+
+  const handleIncrement = () => {
+    setCount((prev) => prev + 1);
+    if (count === 2) {
+      setImage("https://avatars.githubusercontent.com/u/71830780?v=4");
+    }
+  };
+
+  const handleDecrement = () => {
+    setCount((prev) => prev - 1);
+  };
+
+  const handleBgColor = () => {
+    setBgColor("blue");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={handleBgColor}>
+      <View
+        style={{
+          backgroundColor: bgColor,
+          height: "100%",
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text onPress={handleDecrement} style={styles.text}>
+          {count}
+        </Text>
+        <Button title="somar" onPress={handleIncrement} />
+        <Image
+          source={{
+            uri: image,
+          }}
+          style={styles.image}
+        />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 50,
+  },
+  text: {
+    fontSize: 20,
+    padding: 5,
   },
 });
